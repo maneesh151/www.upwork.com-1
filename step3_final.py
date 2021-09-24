a0a179506a97b7bb566b9e3a00c3e003d2603a62
Maneesh Mahajan <maneesh151@gmail.com>
2021-09-24 07:44:54 +0530

    wip

--- step3_final.py
+++ step3_final.py
@@ -1,18 +1,36 @@
 from step2_pdf_txt import output_folder
 from pathlib import Path, PurePath
-import json
+import re
 from logging_code import logger
 
-
 path = Path(output_folder).rglob("*.txt")
+_amt = re.compile(r'"\d*,?\d*\.\d{2}"')
+
+header = {
+    "parcel_no": re.compile(r"\w{3}-\w{3}-\w{3}"),
+    "owner_name": re.compile(r"(\D)"),
+    "cy_tax": _amt,
+    "cy_penalty": _amt,
+    "cy_pubcosts": _amt,
+    "py_tax": _amt,
+    "py_penalty_interest": _amt,
+    "tot_dlnqt_amt": _amt,
+}
 
 # json_output = input_file.replace(".csv", ".json")
 for text_file in [file for file in path]:
 
     with open("final_file.txt", "w") as outfile:
-        header_list = []
-        start_string = ""
-        with open(input) as reader:
+        with open(text_file) as reader:
             for line in reader:
-                start_string == re.match(r"w{3}-w{3}-w{3}", line)
-                header_list += start_string
+                if re.search(header["parcel_no"], line):
+                    for head, pattern in header.items():
+                        # print(head, pattern.finditer(line))
+                        line
+
+                    # parcel_no = re.compile(r"\w{3}-\w{3}-\w{3}")
+                    # for m in parcel_no.finditer(line):
+                    #     # print(line)
+                    #     print(m.start(), m.end(), m.group())
+                    #     i=m.end()
+                    #     line=line[i:]
